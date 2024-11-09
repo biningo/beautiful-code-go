052831561d0232e294163be8a522d6bb27366aa1
biningo <biningo.cn@gmail.com>
2024-11-09 22:09:28 +0800

    exit func

---
+++ exit_func/exit_func.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
+
+/**
+nsq https://github.com/nsqio/nsq
+*/
+
+func demo1() error {
+	time.Sleep(time.Second)
+	return errors.New("demo1 error")
+}
+
+func demo2() error {
+	fmt.Println("demo2")
+	return nil
+}
+
+func main() {
+	exitCh := make(chan error)
+	var once sync.Once
+	exitFunc := func(err error) {
+		once.Do(func() {
+			if err != nil {
+				log.Println("error:" + err.Error())
+				exitCh <- err
+			}
+		})
+	}
+
+	go exitFunc(demo1())
+	go exitFunc(demo2())
+
+	err := <-exitCh
+	fmt.Println("done! error:" + err.Error())
+}
